faea47076987d93b01679e96ce5eda9fcb28b26a
yusuf.ogundiji <yusuf.ogundiji@SOLDEV>
2024-11-11 13:13:37 +0100

    Add project files.

---
+++ AssetsManagementSystem.Dormain/Assets.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssetsManagementSystem.Dormain
+{
+    internal class Assets
+    {
+    }
+}
---
+++ AssetsManagementSystem.Dormain/common/BaseDormainEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AssetsManagementSystem.Dormain.common
+{
+    public abstract class BaseDormainEntity
+    {
+        [Key, Required]
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
+    }
+}
